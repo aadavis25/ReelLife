d81922194d0c4fa7061e96aad9e8ecb0839154c7
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-06 03:10:53 -0400

    j

--- images.php
+++ images.php
@@ -147,7 +147,7 @@ if ($user_id) {
 
               $class = ($i++ % 4 === 0) ? 'first-column' : '';
           ?>
-          <li style="background: url(<?php echo he($picture); ?>) no-repeat; background-size:200px; height:200px;" class="<?php echo $class; ?>">
+          <li style="background: url(<?php echo he($picture); ?>) no-repeat; background-size:400px; height:200px;" class="<?php echo $class; ?>">
             <a href="<?php echo he($link); ?>" target="_top"></a>
           </li>
           <?php
