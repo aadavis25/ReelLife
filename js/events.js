fa43ce88782aca54a554d3c0a26c9d8a881073ec
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-05 23:39:10 -0400

    fsa

--- js/events.js
+++ js/events.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
 
     $("#reveal").click(function(){
     var ul = $(this).next("ul");
+    console.log(ul);
     if (ul.is(":hidden")) {
         ul.slideDown();
     } else {
         ul.slideUp();
     }
-});
+	});
 });​
