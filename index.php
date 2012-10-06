9fe71fcb4ce8ac206bdb784da62f575c5b6fcc3c
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-05 21:20:27 -0400

    lala

--- index.php
+++ index.php
@@ -42,6 +42,7 @@
               <li class="active"><a href="#">Home</a></li>
               <li><a href="#images">Images</a></li>
               <li><a href="#contact">Contact</a></li>
+              <li><div class="fb-login-button" data-scope="user_likes,user_photos"></div></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div>
@@ -50,7 +51,8 @@
 
     <div class="container">
 
-      <h1>Welcome to Reelife</h1>
+
+      <h1>Welcome to Reelife, <strong><?php echo he(idx($basic, 'name')); ?></strong></h1>
       <h2>You have 7 seconds... make it count</h2>
       <div>what exactly is Reelife?
         <ul id="reveal">
@@ -79,6 +81,45 @@
     <script src="js/bootstrap-typeahead.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
     <script src="js/events.js"></script>
-    </script>  
+    </script> 
+    <?php
+
+// Provides access to app specific values such as your app id and app secret.
+// Defined in 'AppInfo.php'
+require_once('AppInfo.php');
+
+// Enforce https on production
+if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+  header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+  exit();
+}
+
+// This provides access to helper functions defined in 'utils.php'
+require_once('utils.php');
+
+require_once('sdk/src/facebook.php');
+
+$facebook = new Facebook(array(
+  'appId'  => AppInfo::appID(),
+  'secret' => AppInfo::appSecret(),
+  'sharedSession' => true,
+  'trustForwarded' => true,
+));
+
+$user_id = $facebook->getUser();
+if ($user_id) {
+
+  try {
+    // Fetch the viewer's basic information
+    $basic = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    // If the call fails we check if we still have a user. The user will be
+    // cleared if the error is because of an invalid accesstoken
+    if (!$facebook->getUser()) {
+      header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+      exit();
+    }
+  }
+ 
   </body>
 </html>
