de0e8301b5e70b9dd1ebd89e2071de99320eef1f
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-06 09:54:08 -0400

    x

--- images.php
+++ images.php
@@ -124,7 +124,7 @@ if ($user_id) {
       <span><strong><h3><?php echo he(idx($basic, 'name')); ?></strong></h3></span>
       </div>
       <?php } ?>
-    <span class="span10 offset3">
+    <span class="span10 offset2">
       <div id="gallery"><h3>Check out your ReelLife</h3>
         <ul>
           <?php
