62333ce7a6175c48a687cadcaf514cb5712c0bb0
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-06 09:20:49 -0400

    x

--- index.php
+++ index.php
@@ -73,11 +73,12 @@ if ($user_id) {
       <div class="navbar-inner">
         <div class="container">
           <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span><img src="/ico/favicon.ico"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-          <span><img src="/icon/favicon.ico"></span>
+
           <a class="brand" href="#">ReelLife</a>
           <div class="nav-collapse collapse">
             <ul class="nav">
