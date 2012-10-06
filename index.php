cd0ca434b218071f7485d58a5af25e3537ed6bdb
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-05 21:50:30 -0400

    bla

--- index.php
+++ index.php
@@ -80,6 +80,14 @@
     <script src="js/bootstrap-typeahead.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript"></script>
     <script src="js/events.js"></script>
+    <script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
     </script> 
     <?php
 
