a3ad144b75e5ea162b0aba813f674accc2f5833a
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-06 13:10:14 -0400

    x

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if ($user_id) {
 
       <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
-        <div class="container" style:"margin-top:25px;">
+        <div class="container" style="margin-top:25px;">
           <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
