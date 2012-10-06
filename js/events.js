5f2ad6a1faea8c267e8bf93418b04162dc0880a5
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-05 23:23:13 -0400

    fsda

--- js/events.js
+++ js/events.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
 
 	$('#reveal').on('click', 'h3', null, function (e) {
 		alert('woo');
-		$($(this).children('ul')).toggle('medium');
+		$(this).children('.hidden').toggle('medium');
 	});
 });
