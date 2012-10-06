6686da9bd81c346ea362394b61162d4d8b287632
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-05 23:44:10 -0400

    faf

--- js/events.js
+++ js/events.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
 
     $("#reveal").click(function(){
-    var ul = $(this).next("ul");
-    console.log(ul);
-    if (ul.is(":hidden")) {
-        ul.slideDown();
-    } else {
-        ul.slideUp();
-    }
-	})
+    	var ul = $(this).next("ul");
+    	console.log(ul);
+    	if (ul.is(":hidden")) {
+        	ul.slideDown();
+    	} 
+    	else {
+        	ul.slideUp();
+    	}	
+	});
 });​
