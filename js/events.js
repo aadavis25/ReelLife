a13027a062df95f82bad267853dc3621f9be6edb
aaronmd <aaronmd@aaronmd-ThinkPad-W510.(none)>
2012-10-06 06:25:07 -0400

    x

--- js/events.js
+++ js/events.js
@@ -11,28 +11,8 @@ $(".reveal").each(function(element){
             }
         });
     });
-var cont_left = $(".container").position().left;
-        $("a img").hover(function() {
-            // hover in
-            $(this).parent().parent().css("z-index", 1);
-            $(this).animate({
-               height: "100%",
-               width: "100%"
-            }, "fast");
-        }, function() {
-            // hover out
-            $(this).parent().parent().css("z-index", 0);
-            $(this).animate({
-                height: "50%",
-                width: "50%"
-            }, "fast");
-        });
 
-        $(".img").each(function(index) {
-            var left = (index * 160) + cont_left;
-            $(this).css("left", left + "px");
-        });
-
-        $("a img").click(function() {
-        	$(this).lightbox()
-});
+    $(function() {
+        $('#gallery a').lightBox();
+    });
+    
